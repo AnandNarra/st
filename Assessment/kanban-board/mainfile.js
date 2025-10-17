eff2cee653f43f6960e80356a64b86dc571fe0f3
AnandNarra <narra9505@gmail.com>
2025-10-17 19:39:09 +0530

    added the assessment folder to the git flie

---
+++ Assessment/kanban-board/mainfile.js
@@ -0,0 +1,126 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const createBacklog = document.getElementById("create-backlog-btn");
+
+    const backlogContent = document.getElementById("backlog-content");
+
+
+    createBacklog.addEventListener("click", () => {
+
+        const newBacklogForm = document.createElement("form");
+        newBacklogForm.className = "backlog-from";
+
+
+        //title
+        const backlogFromContainer = document.createElement("div")
+
+        const lable = document.createElement("label");
+        lable.setAttribute("for", "backlogTitle");
+        lable.textContent = "Backlog Title";
+
+        const input = document.createElement("input")
+        input.setAttribute("id", "backlogTitle")
+        input.setAttribute("placeholder", "enter the backlog title...");
+        input.setAttribute("type", "text");
+
+
+        backlogFromContainer.appendChild(lable);
+        backlogFromContainer.appendChild(input);
+
+        // Description 
+        const descriptionFromContainer = document.createElement("div")
+
+        const descriptionlable = document.createElement("label");
+        descriptionlable.setAttribute("for", "backlogDescription");
+        descriptionlable.textContent = "Description";
+
+        const descriptioninput = document.createElement("textarea")
+        descriptioninput.setAttribute("id", "backlogDescription")
+        descriptioninput.setAttribute("placeholder", "Enter Backlog Description...");
+
+        descriptionFromContainer.appendChild(descriptionlable);
+        descriptionFromContainer.appendChild(descriptioninput);
+
+        //buttons
+
+        const actions = document.createElement("div");
+
+        const createBacklogButton = document.createElement("button");
+        createBacklogButton.setAttribute("type", "submit");
+        createBacklogButton.textContent = "Create Backlog";
+
+        const cancleBtn = document.createElement("button")
+        cancleBtn.type = "button"
+        cancleBtn.textContent = "Cancle"
+
+        actions.appendChild(createBacklogButton)
+        actions.appendChild(cancleBtn);
+
+
+        // adding in to from 
+        newBacklogForm.appendChild(backlogFromContainer);
+        newBacklogForm.appendChild(descriptionFromContainer);
+        newBacklogForm.appendChild(actions);
+
+        function saveTaskToLocalStorage(section , task){
+ 
+
+            const allTasks = localStorage.getItem("KanbanTasks") || {};
+
+            if(!allTasks[section]){
+
+                return allTasks[section] =[];
+            }
+            allTasks[section].push(task)
+
+            localStorage.setItem("kanbanTasks" , allTasks)
+            
+        }
+
+
+        newBacklogForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const title = input.value.trim();
+
+            const description = descriptioninput.value.trim();
+
+            if (!title) {
+                alert("value ni enter cheuuu ra langakoduka..");
+                return;
+            }
+
+            const task = {
+                title,
+                description,
+                createAt: new Date().toISOString(),
+            }
+
+            const section = "Backlogs";
+
+            console.log(task);
+
+            saveTaskToLocalStorage(section, task)
+
+           
+
+           
+
+           
+        })
+
+   
+
+        backlogContent.appendChild(newBacklogForm);
+        input.focus();
+
+
+    
+
+
+    });
+
+
+    
+
+
+});

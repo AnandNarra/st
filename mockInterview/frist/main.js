1b5e06f7555356773d760cd7c5bac5a79a45f32a
AnandNarra <narra9505@gmail.com>
2025-10-27 11:53:19 +0530

    added mockInterview floder in to git

---
+++ mockInterview/frist/main.js
@@ -0,0 +1,18 @@
+let a= 5;
+var b=6;
+const c="anand";
+
+function hello(){
+
+    console.log("anand")
+}
+
+hello()
+
+const anand =() =>{
+    d = a+b+c;
+    console.log("arrow function" +" "+d);
+}
+
+
+
